d827bb0c9f49157bf7502802ad26570f117440d1
Tristan Murphy <72839119+inflectrix@users.noreply.github.com>
2024-02-21 16:35:30 +0000

    implement json des

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,59 @@
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
+use js_sys::Error;
 
 #[derive(Serialize, Deserialize)]
 pub enum Dict {
     String(String),
     Int(i32),
     Float(f32),
-    List(Vec<Option<AnyData>>),
-    Object(HashMap<String, Option<AnyData>>)
+    List(Vec<Option<Dict>>),
+    Object(HashMap<String, Option<Dict>>)
 }
+
+type JsResult = Result<JsValue, Error>;
 
 // no need to deserialize json bc of js but still gonna do it anyway
 #[wasm_bindgen]
-pub fn des_json(s: String) -> Dict {
+pub fn des_json(s: String) -> JsResult {
+    let v: Dict = serde_json::from_str(&s)
+        .map_err(|_| Error::new("Invalid JSON"))?;
+
+    Ok(serde_wasm_bindgen::to_value(&v).unwrap())
+}
+
+#[wasm_bindgen]
+pub fn des_toml(s: String) -> JsResult {
 
 }
 
 #[wasm_bindgen]
-pub fn des_toml(s: String) -> Dict {
+pub fn des_ron(s: String) -> JsResult {
 
 }
 
 #[wasm_bindgen]
-pub fn des_ron(s: String) -> Dict {
+pub fn des_yaml(s: String) -> JsResult {
 
 }
 
 #[wasm_bindgen]
-pub fn des_yaml(s: String) -> Dict {
+pub fn des_pickle(s: String) -> JsResult {
 
 }
 
 #[wasm_bindgen]
-pub fn des_pickle(s: String) -> Dict {
+pub fn des_qs(s: String) -> JsResult {
 
 }
 
 #[wasm_bindgen]
-pub fn des_qs(s: String) -> Dict {
+pub fn des_lexpr(s: String) -> JsResult {
 
 }
 
 #[wasm_bindgen]
-pub fn des_lexpr(s: String) -> Dict {
-    
-}
-
-#[wasm_bindgen]
-pub fn convert_to_all(d: Dict) -> HashMap<String, String> {
+pub fn convert_to_all(d: JsValue) -> JsResult {
 
 }
