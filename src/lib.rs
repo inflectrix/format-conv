69331a6a88d3a723ddb21776c29fc13f4be72114
Tristan Murphy <72839119+inflectrix@users.noreply.github.com>
2024-02-21 16:56:07 +0000

    implement the rest

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,14 @@
-use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use wasm_bindgen::prelude::*;
 use js_sys::Error;
-
-#[derive(Serialize, Deserialize)]
-pub enum Dict {
-    String(String),
-    Int(i32),
-    Float(f32),
-    List(Vec<Option<Dict>>),
-    Object(HashMap<String, Option<Dict>>)
-}
+use toml::Value;
 
 type JsResult = Result<JsValue, Error>;
 
 // no need to deserialize json bc of js but still gonna do it anyway
 #[wasm_bindgen]
 pub fn des_json(s: String) -> JsResult {
-    let v: Dict = serde_json::from_str(&s)
+    let v: Value = serde_json::from_str(&s)
         .map_err(|_| Error::new("Invalid JSON"))?;
 
     Ok(serde_wasm_bindgen::to_value(&v).unwrap())
@@ -25,35 +16,57 @@ pub fn des_json(s: String) -> JsResult {
 
 #[wasm_bindgen]
 pub fn des_toml(s: String) -> JsResult {
+    let v: Value = (&s)
+        .parse()
+        .map_err(|_| Error::new("Invalid TOML"))?;
 
+    Ok(serde_wasm_bindgen::to_value(&v).unwrap())
 }
 
 #[wasm_bindgen]
 pub fn des_ron(s: String) -> JsResult {
+    let v: Value = ron::from_str(&s)
+        .map_err(|_| Error::new("Invalid RON"))?;
 
+    Ok(serde_wasm_bindgen::to_value(&v).unwrap())
 }
 
 #[wasm_bindgen]
 pub fn des_yaml(s: String) -> JsResult {
+    let v: Value = serde_yaml::from_str(&s)
+        .map_err(|_| Error::new("Invalid YAML"))?;
 
-}
-
-#[wasm_bindgen]
-pub fn des_pickle(s: String) -> JsResult {
-
+    Ok(serde_wasm_bindgen::to_value(&v).unwrap())
 }
 
 #[wasm_bindgen]
 pub fn des_qs(s: String) -> JsResult {
+    let v: Value = serde_qs::from_str(&s)
+        .map_err(|_| Error::new("Invalid QString"))?;
 
+    Ok(serde_wasm_bindgen::to_value(&v).unwrap())
 }
 
 #[wasm_bindgen]
 pub fn des_lexpr(s: String) -> JsResult {
+    let v: Value = serde_lexpr::from_str(&s)
+        .map_err(|_| Error::new("Invalid LExpr"))?;
 
+    Ok(serde_wasm_bindgen::to_value(&v).unwrap())
 }
 
 #[wasm_bindgen]
 pub fn convert_to_all(d: JsValue) -> JsResult {
+    let v: Value = serde_wasm_bindgen::from_value(d).unwrap();
 
+    let mut m = HashMap::new();
+
+    m.insert("json", serde_json::to_string(&v).unwrap());
+    m.insert("toml", toml::to_string(&v).unwrap());
+    m.insert("ron", ron::to_string(&v).unwrap());
+    m.insert("yaml", serde_yaml::to_string(&v).unwrap());
+    m.insert("qstr", serde_qs::to_string(&v).unwrap());
+    m.insert("lexpr", serde_lexpr::to_string(&v).unwrap());
+
+    Ok(serde_wasm_bindgen::to_value(&m).unwrap())
 }
