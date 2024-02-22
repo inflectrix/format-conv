0c3cc655c2d396dc753d12409b7fe4faaf4542d1
inflectrix <inflectrix@users.noreply.github.com>
2024-02-22 12:31:24 +0000

    deploy: bdaad2977f2ff36558bd48bfdf61e747432311a4

---
+++ format_conv.d.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} s
+* @returns {any}
+*/
+export function des_json(s: string): any;
+/**
+* @param {string} s
+* @returns {any}
+*/
+export function des_toml(s: string): any;
+/**
+* @param {string} s
+* @returns {any}
+*/
+export function des_ron(s: string): any;
+/**
+* @param {string} s
+* @returns {any}
+*/
+export function des_yaml(s: string): any;
+/**
+* @param {string} s
+* @returns {any}
+*/
+export function des_qs(s: string): any;
+/**
+* @param {string} s
+* @returns {any}
+*/
+export function des_lexpr(s: string): any;
+/**
+* @param {any} d
+* @returns {any}
+*/
+export function convert_to_all(d: any): any;
---
+++ format_conv.js
@@ -0,0 +1,4 @@
+import * as wasm from "./format_conv_bg.wasm";
+import { __wbg_set_wasm } from "./format_conv_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./format_conv_bg.js";
